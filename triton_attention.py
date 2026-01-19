165fe59cbaa7c5f51e7040225707d710449c4814
Naren Sarayu Manoj <manoj.narens@gmail.com>
2026-01-19 12:08:25 -0500

    tile size

--- triton_attention.py
+++ triton_attention.py
@@ -184,8 +184,8 @@ def flash_fwd_kernel(
 class TritonAttention(torch.autograd.Function):
     @staticmethod
     def forward(ctx, query, key, value, is_causal=False):
-        ctx.Q_TILE_SIZE = 16
-        ctx.KV_TILE_SIZE = 16
+        ctx.Q_TILE_SIZE = 128
+        ctx.KV_TILE_SIZE = 128
         assert ctx.Q_TILE_SIZE == ctx.KV_TILE_SIZE
         ctx.D = query.shape[-1]
         ctx.scale = ctx.D ** -0.5
